ca9714c339f97642fa9dbde3098b3b5e47c47cf8
Simon Maspero <simon.maspero@voxco.com>
2014-12-12 15:49:32 -0500

    Action bar in maps activity

--- app/src/main/java/com/njzk2/wtwtl/MapsActivity.java
+++ app/src/main/java/com/njzk2/wtwtl/MapsActivity.java
@@ -1,14 +1,14 @@
 package com.njzk2.wtwtl;
 
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity {
+public class MapsActivity extends ActionBarActivity {
 
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
 
